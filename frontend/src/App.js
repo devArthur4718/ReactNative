f9b106892d90c8eebef25854e88d0df766ac3f30
devarthur4718 <Dev4718@face>
2020-03-26 20:16:21 -0300

    project assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState} from 'react';
+
+import Header from './Header';
 
 function App() {
-  return (
+  let [counter, setCounter] = useState(0);
 
-    <h1>Hello Word!!!!</h1>
+  //Retorna um array com duas posicoes com [valor, funcDeAtualizacao]
+
+  function increment(){
+    setCounter(counter + 1); 
+   
+  }
+
+
+  return (
+    <div>
+         <Header > Semana Omnistack {counter} </Header>
+         <button onClick={increment}>Incrementar</button>
+    </div>
  
   );
 }
-
+ 
 export default App;
+ 
---
+++ frontend/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+
+export default function Header(props) {
+    return(
+        <header>
+            <h1>{props.children}</h1>
+        </header>
+    );
+}
+
+
+ 
