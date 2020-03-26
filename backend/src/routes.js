96c45eb61c9ece099274a18b5f6813f19d332952
devarthur4718 <Dev4718@face>
2020-03-26 14:04:22 -0300

    added connection file for dabatase

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,24 @@
 const express = require('express'); 
-
+const crypto = require('crypto')
+const connection = require('./database/connection');
 const routes = express.Router(); 
 
-routes.post('/users', (request, response) => {
-    const body = request.body; 
+routes.post('/ongs',  async (request, response) => {
+    const {name, email, whatsapp, city, uf} = request.body; 
+    const id = crypto.randomBytes(4).toString('HEX');
 
-    console.log(body);
+    await connection('tabOngs').insert({
+        id,
+        name,
+        email,
+        whatsapp, 
+        city,
+        uf,
+    })
 
-    return response.json({
-        evento : "Semana OmniStack 11.0",
-        aluno : "Arthur Gomes Silva"
- 
-     });
+
+    return response.json({id});
+
 });   
 
 module.exports = routes;
