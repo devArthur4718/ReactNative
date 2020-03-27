751e4412766f8acbfcd1869b80ba28905260e271
devarthur4718 <Dev4718@face>
2020-03-27 04:01:14 -0300

    added register router

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,10 +1,12 @@
 import React from 'react'; 
+import {} from 'react-router-dom'; 
 import {FiLogIn} from 'react-icons/fi'
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function Logon() { 
     return (
@@ -18,10 +20,10 @@ export default function Logon() {
                <input placeholder="Sua ID"/>
                <button className="button" type="submit">Entrar</button>
 
-               <a href="/register"> 
+               <Link to="/register"> 
                <FiLogIn size={16} color="#E02041" />
                     Não tenho cadastro
-               </a>
+               </Link>
            </form>
            </section>
 
