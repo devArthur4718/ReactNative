d773f0c0e3dd7d75d1b6e8d6b60462b1862b3eb2
Arthur Gomes <devarthur4718@gmail.com>
2020-03-29 17:28:42 -0300

    utils

--- backend/src/controller/OngController.js
+++ backend/src/controller/OngController.js
@@ -1,5 +1,5 @@
 const connection = require('../database/connection');
-const crypto = require('crypto')
+const customId = require('../utils/generateUniqueId.js');
 
 module.exports = { 
 
@@ -10,7 +10,7 @@ module.exports = {
 
     async create(request, response){
         const {name, email, whatsapp, city, uf} = request.body; 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = customId.generateUniqueId();
 
         await connection('ongs').insert({
             id,
