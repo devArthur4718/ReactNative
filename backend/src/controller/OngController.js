7d72e6934888ac87a65b3929ba26c503f5c751eb
Arthur Gomes <devarthur4718@gmail.com>
2020-03-29 18:02:42 -0300

    fixed an error with custom id

--- backend/src/controller/OngController.js
+++ backend/src/controller/OngController.js
@@ -1,16 +1,15 @@
 const connection = require('../database/connection');
-const customId = require('../utils/generateUniqueId.js');
+const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId')
 
 module.exports = { 
-
     async index(request,response){
         const ongs = await connection('ongs').select('*');
         return response.json(ongs);
     },
-
     async create(request, response){
         const {name, email, whatsapp, city, uf} = request.body; 
-        const id = customId.generateUniqueId();
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
--- backend/src/utils/generateUniqueId.js
+++ backend/src/utils/generateUniqueId.js
@@ -1,7 +1,6 @@
 const crypto = require('crypto');
 
 
-module.exports = 
-    function generateUniqueId(){
-        return crypto.randomBytes(4).toString('HEX');
-    }
+module.exports = function generateUniqueId() {
+    return crypto.randomBytes(4).toString('HEX');
+}
