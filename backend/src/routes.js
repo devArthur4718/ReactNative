4a2705af347ec977c8c639880471858553561a04
Arthur Gomes <devarthur4718@gmail.com>
2020-03-29 13:45:18 -0300

    added validation using celebrate

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {celebrate, Segments, Joi} = require('celebrate')
 const OngController = require('./controller/OngController');
 const IncidentController = require('./controller/IncidentController');
 const ProfileController = require('./controller/ProfileController');
@@ -10,9 +11,23 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
 
-routes.get('/profile', ProfileController.index);
+routes.post('/ongs', celebrate({
+    [Segments.BODY] : Joi.object().keys({
+        name : Joi.string().required(), 
+        email : Joi.string().required().email(), 
+        whatsapp : Joi.number().required().min(10).max(11), 
+        city : Joi.string().required(), 
+        uf: Joi.string().required().length(2), 
+
+    })
+}),OngController.create);
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS] : Joi.object().keys({
+        authorization : Joi.string().required(),
+    })
+}),ProfileController.index);
 
 routes.post('/incidents', IncidentController.create);
 routes.get('/incidents', IncidentController.index );
